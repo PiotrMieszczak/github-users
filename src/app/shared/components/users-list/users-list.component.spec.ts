e13732ec790932451412737875bf1a7f62944b08
pmieszczak <piotr.mieszczak@syzygy.pl>
2022-05-24 11:15:46 +0200

    refactor: 💡 Added virtual scroll to users list

--- src/app/shared/components/users-list/users-list.component.spec.ts
+++ src/app/shared/components/users-list/users-list.component.spec.ts
@@ -2,13 +2,24 @@ import { Spectator, createComponentFactory } from '@ngneat/spectator/jest';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { UsersListComponent } from './users-list.component';
 import { RouterTestingModule } from '@angular/router/testing';
+import { TuiScrollbarModule } from '@taiga-ui/core';
+import { ScrollingModule } from '@angular/cdk/scrolling';
+import { TuiAvatarModule, TuiIslandModule } from '@taiga-ui/kit';
 
 describe('UsersListComponent', () => {
   let spectator: Spectator<UsersListComponent>;
 
   const createComponent = createComponentFactory({
     component: UsersListComponent,
-    imports: [FormsModule, ReactiveFormsModule, RouterTestingModule],
+    imports: [
+      FormsModule,
+      ReactiveFormsModule,
+      RouterTestingModule,
+      TuiScrollbarModule,
+      ScrollingModule,
+      TuiAvatarModule,
+      TuiIslandModule,
+    ],
   });
 
   beforeEach(() => {
